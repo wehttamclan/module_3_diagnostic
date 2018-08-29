ef10e44f83c0ca399e1cce4d8b81c567af9d7ae2
Matthew Peters <JiveTurkey@MattBook-Pro.local>
2018-08-29 11:08:17 -0600

    final commit - adds attributes to Station and view

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,8 +1,12 @@
 class Station
-  attr_reader :name
+  attr_reader :name, :fuel_type, :address, :distance, :access_time
 
   def initialize(info)
     @name = info[:station_name]
+    @address = info[:street_address]
+    @fuel_type = info[:fuel_type_code]
+    @distance = info[:distance]
+    @access_time = info[:access_days_time]
   end
 
 end
--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -36,14 +36,3 @@ feature "User can visit root page" do
     end
   end
 end
-
-=begin
-As a user
-When I visit "/"
-And I fill in the search form with 80203 (Note: Use the existing search form)
-And I click "Locate"
-Then I should be on page "/search"
-Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-And the stations should be limited to Electric and Propane
-And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
-=end
